bb90cd155e335f7317b0b41d6bd7e2bd407552c9
Napster <rome.1205h@gmail.com>
2023-05-21 23:50:55 -0500

    complete js

---
+++ script.js
@@ -0,0 +1,10 @@
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const con = document.querySelector('.container');
+
+open.addEventListener( 'click', () => con.classList.add('show-nav') )
+
+close.addEventListener( 'click', () => alert('hi') )
+
+
+console.log('con', con)
